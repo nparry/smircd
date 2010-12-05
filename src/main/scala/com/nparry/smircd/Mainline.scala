a9a882846d106d0f544f62d17f4311d82889b74f
Nathan Parry <nparry@gmail.com>
2010-12-04 20:28:00 -0500

    Change log level on the fly when running in sbt

--- src/main/scala/com/nparry/smircd/Mainline.scala
+++ src/main/scala/com/nparry/smircd/Mainline.scala
@@ -9,23 +9,51 @@ import org.jboss.netty.logging._
 import ch.qos.logback.classic.Logger
 import ch.qos.logback.classic.Level
 
+import scala.annotation.tailrec
+
 object Mainline {
+  val logger = grizzled.slf4j.Logger(this.getClass())
+
   case class Args(
     port: Int = 6667,
-    logLevel: String = "INFO",
+    logLevel: String = Level.INFO.toString(),
     inSbt: Boolean = false)
 
-  def processArgs(current: Args, args: List[String]): Args = {
+  @tailrec def processArgs(current: Args, args: List[String]): Args = {
     args match {
       case "--port" :: port :: rest => processArgs(current.copy(port=port.toInt), rest)
       case "--logLevel" :: level :: rest => processArgs(current.copy(logLevel=level), rest)
       case "--inSbt" :: rest => processArgs(current.copy(inSbt=true), rest)
       case unknown :: rest => {
-        System.err.println("Unknown argument: " + unknown)
+        logger.warn("Unknown argument: " + unknown)
         processArgs(current, rest)
 
       }
       case Nil => current
+    }
+  }
+
+  val ChangeLogLevel = """\s*level\s*=?\s*(\S+)\s*""".r
+  @tailrec def waitForQuitInSbt(): Unit = {
+    Console.readLine() match {
+      case "" => return
+      case ChangeLogLevel(level) => {
+        Console.print("Changing log level to " + level + "\n")
+        setLogLevel(level)
+        waitForQuitInSbt()
+      }
+      case _ => waitForQuitInSbt()
+    }
+  }
+
+  def setLogLevel(level: String) = {
+    try {
+      org.slf4j.LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME) match {
+        case l: Logger => l.setLevel(Level.toLevel(level))
+        case a: Any => throw new IllegalStateException("Unexpected logger type: " + a.getClass())
+      }
+    } catch {
+      case e: Exception => logger.error("Unable to set logging level to " + level + ": " + e.getMessage())
     }
   }
 
@@ -34,17 +62,18 @@ object Mainline {
   def main(args: Array[String]) {
     val arguments = processArgs(Args(), args.toList)
 
-    org.slf4j.LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME) match {
-      case l: Logger => l.setLevel(Level.toLevel(arguments.logLevel))
-      case default => System.err.println("Unable to set logging level")
-    }
+    setLogLevel(arguments.logLevel)
 
     val m = new Mainline(arguments.port)
     m.start()
 
     if (arguments.inSbt) {
-      Console.readLine("Press enter to quit...\n")
-      m.stop
+      try {
+        Console.print("Press enter to quit...\n")
+        waitForQuitInSbt()
+      } finally {
+        m.stop
+      }
     }
     else {
       Runtime.getRuntime().addShutdownHook(new Thread() { override def run {
