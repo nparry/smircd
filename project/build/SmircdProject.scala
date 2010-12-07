4f5d78ff065c80acc045db29c29c5858234bcfc4
Nathan Parry <nparry@gmail.com>
2010-12-06 21:17:45 -0500

    Use proguard to build an executable uberjar

--- project/build/SmircdProject.scala
+++ project/build/SmircdProject.scala
@@ -1,6 +1,6 @@
 import sbt._
 
-class SmircdProject(info: ProjectInfo) extends DefaultProject(info) {
+class SmircdProject(info: ProjectInfo) extends DefaultProject(info) with ProguardProject {
   val jbossRepo = "JBoss Release Repository" at "http://repository.jboss.org/nexus/content/groups/public/"
 
   val netty = "org.jboss.netty" % "netty" % "3.2.3.Final"
@@ -10,5 +10,20 @@ class SmircdProject(info: ProjectInfo) extends DefaultProject(info) {
   val grizzled = "org.clapper" %% "grizzled-slf4j" % "0.3.2"
 
   val specs = "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5" % "test"
+
+  override val mainClass = Some("com.nparry.smircd.Mainline")
+
+  // Until https://github.com/nuttycom/sbt-proguard-plugin/pull/2 is fixed
+  val macJdkBase = new java.io.File(System.getProperty("java.home")).getParent()
+  def macJdkClassesPath = Path.fromFile(macJdkBase) / "Classes"/ "classes.jar"
+  override def proguardLibraryJars = super.proguardLibraryJars +++ (macJdkClassesPath :PathFinder)
+
+  override def proguardInJars = super.proguardInJars +++ scalaLibraryPath
+  override def proguardOptions = List(
+    proguardKeepMain("com.nparry.smircd.Mainline"),
+    "-keep class com.nparry.smircd.**",
+    "-dontobfuscate",
+    "-dontoptimize",
+    "-forceprocessing")
 }
 
