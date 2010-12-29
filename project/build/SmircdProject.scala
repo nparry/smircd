86d8452459c4d21f6080234c1a6199e406251933
Nathan Parry <nparry@gmail.com>
2010-12-28 20:33:05 -0500

    Split into server and ui projects to prepare for a barebones client

--- project/build/SmircdProject.scala
+++ project/build/SmircdProject.scala
@@ -1,29 +1,35 @@
 import sbt._
 
-class SmircdProject(info: ProjectInfo) extends DefaultProject(info) with ProguardProject {
-  val jbossRepo = "JBoss Release Repository" at "http://repository.jboss.org/nexus/content/groups/public/"
+class SmircdProject(info: ProjectInfo) extends ParentProject(info) {
 
-  val netty = "org.jboss.netty" % "netty" % "3.2.3.Final"
+  lazy val server = project("server", "SmIRCd Server", new SmircdServerProject(_))
+  lazy val ui = project("ui", "SmIRCd UI", new DefaultWebProject(_))
 
-  val slf4j = "org.slf4j" % "slf4j-api" % "1.6.1"
-  val logback = "ch.qos.logback" % "logback-classic" % "0.9.26"
-  val grizzled = "org.clapper" %% "grizzled-slf4j" % "0.3.2"
+  class SmircdServerProject(info: ProjectInfo) extends DefaultProject(info) with ProguardProject {
+    val jbossRepo = "JBoss Release Repository" at "http://repository.jboss.org/nexus/content/groups/public/"
 
-  val specs = "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5" % "test"
+    val netty = "org.jboss.netty" % "netty" % "3.2.3.Final"
 
-  override val mainClass = Some("com.nparry.smircd.Mainline")
+    val slf4j = "org.slf4j" % "slf4j-api" % "1.6.1"
+    val logback = "ch.qos.logback" % "logback-classic" % "0.9.26"
+    val grizzled = "org.clapper" %% "grizzled-slf4j" % "0.3.2"
 
-  // Until https://github.com/nuttycom/sbt-proguard-plugin/pull/2 is fixed
-  val macJdkBase = new java.io.File(System.getProperty("java.home")).getParent()
-  def macJdkClassesPath = Path.fromFile(macJdkBase) / "Classes"/ "classes.jar"
-  override def proguardLibraryJars = super.proguardLibraryJars +++ (macJdkClassesPath :PathFinder)
+    val specs = "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5" % "test"
 
-  override def proguardInJars = super.proguardInJars +++ scalaLibraryPath
-  override def proguardOptions = List(
-    proguardKeepMain("com.nparry.smircd.Mainline"),
-    "-keep class com.nparry.smircd.**",
-    "-dontobfuscate",
-    "-dontoptimize",
-    "-forceprocessing")
+    override val mainClass = Some("com.nparry.smircd.Mainline")
+
+    // Until https://github.com/nuttycom/sbt-proguard-plugin/pull/2 is fixed
+    val macJdkBase = new java.io.File(System.getProperty("java.home")).getParent()
+    def macJdkClassesPath = Path.fromFile(macJdkBase) / "Classes"/ "classes.jar"
+    override def proguardLibraryJars = super.proguardLibraryJars +++ (macJdkClassesPath :PathFinder)
+
+    override def proguardInJars = super.proguardInJars +++ scalaLibraryPath
+    override def proguardOptions = List(
+      proguardKeepMain("com.nparry.smircd.Mainline"),
+      "-keep class com.nparry.smircd.**",
+      "-dontobfuscate",
+      "-dontoptimize",
+      "-forceprocessing")
+  }
 }
 
